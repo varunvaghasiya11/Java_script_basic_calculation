5120fd8cffd3944d3b759e0a62224fa82720216a
Varun Vaghasiya <varunvaghasiya@192.168.1.126>
2025-08-19 14:56:42 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+var b = 9;
+let h = 7;
+var area = 0.5*h*b;
+console.log("area of triangle is = ",area);
+
+var cel = 65;
+var f = (cel*9/5)+32;
+console.log("temp is = ",f);
+
+var len = 4;
+var width = 7;
+var area = (width*len);
+console.log("Area of Rectangle =",area);
+
+var r = 5;
+var area = 3.14*r*r;
+console.log("Area of circle =",area);
+
+var a = 3,b = 4,c = 5;
+
+var s1 = (a-b)*(a+b);
+console.log("a^2 - b^2 = ",s1);
+
+var s2 = a*a - 2*a*b + b*b;
+console.log("(a-b)^2 = ",s2);
+
+var s3 = a*a + b*b + c*c + 2*a*b + 2*a*c + 2*b*c;
+console.log("(a+b+c)^2 = ",s3);
+
+var s4 = a*a + b*b + c*c - 2*a*b - 2*a*c + 2*b*c;
+console.log("(a-b-c)^2 = ",s4);
+
+var s5 = (a*a*a) - 3*a*a*b + 3*a*b*b - b*b*b;  
+console.log("(a-b)^3 = ",s5);
+
+ 
